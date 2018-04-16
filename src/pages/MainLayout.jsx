4ab891c6491b6c2aca3ef2da30018b3c787b3586
zido <wuhongxu1208@gmail.com>
2018-04-17 00:06:12 +0800

    update

--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { renderRoutes,matchRoutes } from 'react-router-config'
 import PropTypes  from 'prop-types'
+import { NavLink } from 'react-router-dom'
 export default class MainLayout extends React.Component{
   state  = {
     current: '/',
@@ -13,10 +14,12 @@ export default class MainLayout extends React.Component{
     console.log(matchRoutes(routes,this.props.route.path))
     return (
       <div className="main-container">
+        <div>
+          <NavLink to="/">首页</NavLink> 
+          <NavLink to="/user">用户</NavLink>
+        </div>
         <div className="main-content">
           {renderRoutes(routes)} 
-        </div>
-        <div>
         </div>
       </div>
     )
